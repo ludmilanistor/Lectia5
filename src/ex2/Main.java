968bac0c6ad8c65aee46074ac1efee7fc4f9d141
ludmilanistor <146101325+ludmilanistor@users.noreply.github.com>
2023-10-06 19:42:46 +0300

    Update Main.java

--- src/ex2/Main.java
+++ src/ex2/Main.java
@@ -3,18 +3,12 @@ package ex2;
 public class Main {
     public static void  main (String[] arg) {
         System.out.println("Un șir de numere de la 100 pina la 1000, divizibile la 5 și 6");
-        //if ()
-        //int i =0;
-        int div1 = 5;
-        int div2 = 6;
-
-        for (int i = 100; i<=100; i++){
-
-            //System.out.print("" + i);
-            System.out.println(i%div1 + "");
-            //break;
-            //System.out.println("");
-            //System.out.print(i%div2);
+        int div5 = 5;
+        int div6 = 6;
+        for (int i = 100; i<=1000; i++) {
+            if (i%div5 == 0 && i%div6==0){
+                System.out.print(i);
+                System.out.print(" ");}
         }
     }
 }
