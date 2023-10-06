203b526e86bbbf968c40aecbd6ab352cf368b8ea
ludmilanistor <146101325+ludmilanistor@users.noreply.github.com>
2023-10-06 19:44:15 +0300

    Update SumaSeriilor.java

--- src/ex3/SumaSeriilor.java
+++ src/ex3/SumaSeriilor.java
@@ -3,22 +3,8 @@ package ex3;
 public class SumaSeriilor {
     public static void main (String[] arg){
 
-        int number = 3;
-        for (int i=0; i<=99; i++){
-            System.out.print(i+number+" ");
-            //System.out.print(i+i+" ");//i++
+        for (int i= 1; i<= 99; i++){
+            System.out.print(i++ +" ");
             //break;
-        }
-
-
-//    /// Varianta 2
-//      String number = "A";
-//    StringBuilder stringBuilder = new StringBuilder("A");
-//          for (int i = 1; i<=15; i++) {
-//              // System.out.print(i + number + " ");
-//              //break;
-//              stringBuilder.append(i);
-//              System.out.println(stringBuilder);
-//          }
     }
 }
