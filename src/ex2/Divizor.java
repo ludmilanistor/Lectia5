0844df99bef78eabfb2e3c1ed87bef7c21244983
ludmila.nistor <nistor.ludmila@gmail.com>
2023-10-06 10:53:16 +0300

    Initial commit

---
+++ src/ex2/Divizor.java
@@ -0,0 +1,15 @@
+package ex2;
+
+public class Divizor {
+
+    public int div1;
+    public int div2;
+
+    public int getDiv1() {
+        return div1;
+    }
+
+    public int getDiv2() {
+        return div2;
+    }
+}
---
+++ src/ex2/Main.java
@@ -0,0 +1,20 @@
+package ex2;
+
+public class Main {
+    public static void  main (String[] arg) {
+        System.out.println("Un șir de numere de la 100 pina la 1000, divizibile la 5 și 6");
+        //if ()
+        //int i =0;
+        int div1 = 5;
+        int div2 = 6;
+
+        for (int i = 100; i<=100; i++){
+
+            //System.out.print("" + i);
+            System.out.println(i%div1 + "");
+            //break;
+            //System.out.println("");
+            //System.out.print(i%div2);
+        }
+    }
+}
