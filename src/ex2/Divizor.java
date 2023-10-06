4ac1a58930837582cba7f24398e7dd15dea0df5c
ludmilanistor <146101325+ludmilanistor@users.noreply.github.com>
2023-10-06 19:41:13 +0300

    Update Divizor.java

--- src/ex2/Divizor.java
+++ src/ex2/Divizor.java
@@ -2,14 +2,5 @@ package ex2;
 
 public class Divizor {
 
-    public int div1;
-    public int div2;
 
-    public int getDiv1() {
-        return div1;
-    }
-
-    public int getDiv2() {
-        return div2;
-    }
 }
